b5a07f3eb9e5d671a0348cd2c8af860cc838d025
Dan Perkins <danjonperkins@gmail.com>
2023-04-12 12:16:34 +0100

    clerk setup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,17 @@
-import { type AppType } from "next/dist/shared/lib/utils";
+import { dark } from "@clerk/themes";
+import { ClerkProvider } from "@clerk/nextjs";
+import { type AppType } from "next/app";
 
 import "~/styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+    const appearance = { baseTheme: dark };
+
+    return (
+        <ClerkProvider appearance={appearance} {...pageProps}>
+            <Component {...pageProps} />
+        </ClerkProvider>
+    );
 };
 
 export default MyApp;
