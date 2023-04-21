a1f9ee9f5183352510f482391dd43ece50f8aad7
Dan Perkins <danjonperkins@gmail.com>
2023-04-21 11:02:42 +0100

    fix: welcome text color

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,19 +2,15 @@ import { SignIn } from "@clerk/nextjs";
 import { type NextPage } from "next";
 
 const Home: NextPage = () => {
-    return (
-        <>
-                <div className="text-center">
-                    <h1 className="text-4xl font-bold text-[#19191A]">
-                        Welcome to
-                    </h1>
-                    <h1 className=" text-8xl font-extrabold text-white">
-                        SNET
-                    </h1>
-                </div>
-                <SignIn />
-        </>
-    );
+  return (
+    <>
+      <div className="text-center">
+        <h1 className="text-4xl font-bold text-slate-400">Welcome to</h1>
+        <h1 className=" text-8xl font-extrabold text-white">SNET</h1>
+      </div>
+      <SignIn />
+    </>
+  );
 };
 
 export default Home;
